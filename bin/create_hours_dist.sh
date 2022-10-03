5a5148b143883153e4780da013ef8f51731a14cb
Gregory Petterson <pet03356@morris.umn.edu>
2022-10-02 20:49:26 -0500

    create_hours_dist.sh is now working. It's the same thing as create_username_dist.sh except for cutting the 3rd column essentially.

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Takes a single command line argument that is the dame of a directory
+dir_name="$1"
+
+#assume this script is being called in the project directory. It goes into a subdirectory, which is named after the computer the logs appeared on, of the given arguemnt and cuts out the hours column and then loops to the next subdirectory cutting out the hours logged on that computer. It then counts the repeats and then awks that into an html command. it is then wrapped by the create_hours_dist header and foooter.
+
+# I'm not sure why. But this only loops correctly with the "*". I know that it acts as a wildcard# for the path. But I'm confused because It 
+for compdir in "$dir_name"/*/
+do
+	cut "$compdir"/failed_login_data.txt --delimiter=" " -f3 >> SCRATCH3.txt 
+done
+
+sort SCRATCH3.txt | uniq -c |awk 'match($0, /([0-9]+) ([0-9]+)/, group) {print "data.addRow([\x27" group[2] "\x27, " group[1] "]);"}' >> SCRATCH4.txt
+
+./bin/wrap_contents.sh SCRATCH4.txt html_components/hours_dist "$dir_name"/hours_dist.html
