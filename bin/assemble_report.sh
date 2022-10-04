6c4af9e3591633fe70adf8a27e110a6c5b403f8a
Gregory Petterson <pet03356@morris.umn.edu>
2022-10-03 20:22:53 -0500

    Finished create_country_dist.sh. It loops through the compdir's extracting IP's and then sorts them. After that it joins that file and the file containing the mapped IP'. It cuts out the countries and then sorts it so uniq can get th count before finally awking everything into the correct HTML commands. It the wraps that file int the correct header and footer.

---
+++ bin/assemble_report.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+#Takes a directory containing country_dist.html, hours_dist.html, and username_dist.html
---
+++ bin/create_country_dist.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#Takes a single command line argument.
+dir_name="$1"
+
+for compdir in "$dir_name"/*/
+do
+	cut "$compdir"/failed_login_data.txt --delim=" " -f5 >> cut_IP.txt
+done
+
+sort cut_IP.txt > sorted_cut_IP.txt
+
+join sorted_cut_IP.txt etc/country_IP_map.txt | cut --delim=" " -f2 | sort | uniq -c  \
+	|awk ' match($0, /([0-9]+) (\S+)/, group) {print "data.addRow([\x27" group[2] "\x27, " group[1] "]);"}' >> scratch_data.txt
+
+./bin/wrap_contents.sh scratch_data.txt html_components/country_dist "$dir_name"/country_dist.html
+
+rm cut_IP.txt
+rm sorted_cut_IP.txt
+rm scratch_data.txt
