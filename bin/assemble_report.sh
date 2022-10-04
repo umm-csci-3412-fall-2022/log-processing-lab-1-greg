af1cdf9e23e6c244ba4ac35d2f1969364d264df3
Gregory Petterson <pet03356@morris.umn.edu>
2022-10-03 21:14:21 -0500

    Finished assemble_reports.sh, goes into the directory and cats the contents of the files the other scripts assembled into a single file. Then it wraps it.

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 #Takes a directory containing country_dist.html, hours_dist.html, and username_dist.html
+directory="$1"
+
+# Collects the contents of all the generated files and then wraps them.
+cat "$directory"/country_dist.html "$directory"/hours_dist.html "$directory"/username_dist.html > assembled.txt
+
+./bin/wrap_contents.sh assembled.txt html_components/summary_plots "$directory"/failed_login_summary.html
+
+rm assembled.txt
